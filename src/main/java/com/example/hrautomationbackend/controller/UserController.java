7a13b1d001eedf9a2874e32ae1e3964ecc3ecbf7
SFRMilkTea <bayliz@bk.ru>
2022-10-12 22:30:35 +0700

    создан контролер, добавлен метод "Авторизрция"

---
+++ src/main/java/com/example/hrautomationbackend/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.hrautomationbackend.controller;
+
+import com.example.hrautomationbackend.entity.UserEntity;
+import com.example.hrautomationbackend.exception.UserAlreadyExistException;
+import com.example.hrautomationbackend.exception.UserNotFoundException;
+import com.example.hrautomationbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity authorization(@RequestParam String email) {
+        try {
+            return ResponseEntity.ok(userService.authorization(email));
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+//
+//    @PostMapping
+//    public ResponseEntity registration(@RequestBody UserEntity user) {
+//        try {
+//            userService.registration(user);
+//            return ResponseEntity.ok("Пользователь " + user.getUsername() + " успешно добавлен");
+//        } catch (UserAlreadyExistException e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().body("Произошла ошибка");
+//        }
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity getUser(@RequestParam Long id) {
+//        try {
+//            return ResponseEntity.ok(userService.getUser(id));
+//        } catch (UserNotFoundException e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().body("Произошла ошибка");
+//        }
+//    }
+}
