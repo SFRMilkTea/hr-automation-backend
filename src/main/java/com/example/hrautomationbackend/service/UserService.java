379840689c842d2b227a2ce1637736ab15362a84
SFRMilkTea <bayliz@bk.ru>
2022-10-13 22:12:45 +0700

    изменена авторизация

--- src/main/java/com/example/hrautomationbackend/service/UserService.java
+++ src/main/java/com/example/hrautomationbackend/service/UserService.java
@@ -3,27 +3,27 @@ package com.example.hrautomationbackend.service;
 import com.example.hrautomationbackend.entity.UserEntity;
 import com.example.hrautomationbackend.exception.UserAlreadyExistException;
 import com.example.hrautomationbackend.exception.UserNotFoundException;
-import com.example.hrautomationbackend.model.User;
 import com.example.hrautomationbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private EmailService emailService;
 
-    public UserEntity authorization(String email) throws UserNotFoundException {
-        if (userRepository.findByEmail(email) == null) {
+    public boolean authorization(String email) throws UserNotFoundException {
+        UserEntity user = userRepository.findByEmail(email);
+        if (user == null) {
             throw new UserNotFoundException("Пользователь с данным email не существует");
         } else {
-            return ( userRepository.findByEmail(email));
-            //    вот тут начинаем отправлять код на почту
+            emailService.sendEmail(user);
+            return true;
         }
-        }
+    }
 
     public UserEntity registration(UserEntity user) throws UserAlreadyExistException {
         if (userRepository.findByUsername(user.getUsername()) == null) {
