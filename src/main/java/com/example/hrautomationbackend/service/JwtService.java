159f9acb8757448c34d9adf0fbc64fd8788bfe96
SFRMilkTea <bayliz@bk.ru>
2022-10-20 20:05:21 +0700

    Переименован сервис, перемещена функция получения токенов

--- src/main/java/com/example/hrautomationbackend/service/JwtService.java
+++ src/main/java/com/example/hrautomationbackend/service/JwtService.java
@@ -3,23 +3,34 @@ package com.example.hrautomationbackend.service;
 import com.example.hrautomationbackend.entity.UserEntity;
 import com.example.hrautomationbackend.exception.RefreshTokenIsNotValidException;
 import com.example.hrautomationbackend.exception.UserNotFoundException;
+import com.example.hrautomationbackend.exception.WrongAuthorizationCodeException;
 import com.example.hrautomationbackend.jwt.JwtProvider;
 import com.example.hrautomationbackend.jwt.JwtResponse;
 import com.example.hrautomationbackend.repository.UserRepository;
 import io.jsonwebtoken.Claims;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class JwtTokenService {
+@Service
+public class JwtService {
 
     private final Map<String, String> refreshStorage = new HashMap<>();
     @Autowired
     private JwtProvider jwtProvider;
     @Autowired
     private UserRepository userRepository;
+
+    public JwtResponse getTokens(UserEntity user) throws WrongAuthorizationCodeException {
+            final String accessToken = jwtProvider.generateAccessToken(user);
+            final String refreshToken = jwtProvider.generateRefreshToken(user);
+            refreshStorage.put(user.getEmail(), refreshToken);
+            return new JwtResponse(accessToken, refreshToken);
+        }
+
 
 //    public JwtResponse getAccessToken(@NonNull String refreshToken) {
 //        if (jwtProvider.validateRefreshToken(refreshToken)) {
