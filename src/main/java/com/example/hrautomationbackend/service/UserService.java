526d653623cbb2ddf7b9ba3f89f1732dd628e8f3
SFRMilkTea <bayliz@bk.ru>
2022-10-21 16:47:27 +0700

    Добавлен сервис для работы с разделом "Сотрудники"

--- src/main/java/com/example/hrautomationbackend/service/UserService.java
+++ src/main/java/com/example/hrautomationbackend/service/UserService.java
@@ -1,12 +1,14 @@
 package com.example.hrautomationbackend.service;
 
+import com.example.hrautomationbackend.entity.UserEntity;
 import com.example.hrautomationbackend.exception.UserNotFoundException;
 import com.example.hrautomationbackend.model.User;
 import com.example.hrautomationbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.NoSuchElementException;
+import java.util.*;
+
 @Service
 public class UserService {
 
@@ -21,4 +23,24 @@ public class UserService {
         }
         return User.toModel(userRepository.findById(id).get());
     }
+
+    public List<User> getUsers() {
+        Iterable<UserEntity> users = userRepository.findAll();
+        ArrayList<User> usersModel = new ArrayList<>();
+        for (UserEntity user : users) {
+            usersModel.add(User.toModel(user));
+        }
+        return usersModel;
+
+    }
+
+    public Boolean delete(Long id) throws UserNotFoundException {
+        try {
+            userRepository.deleteById(id);
+        } catch (NoSuchElementException e) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
+        return true;
+    }
+
 }
