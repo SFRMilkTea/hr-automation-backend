b90cce35ac05df88fc5b19c18b1e2b39737695a8
SFRMilkTea <bayliz@bk.ru>
2022-10-31 19:51:56 +0700

    создана ошибка "Вопрос уже существует"

---
+++ src/main/java/com/example/hrautomationbackend/exception/QuestionAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.example.hrautomationbackend.exception;
+
+public class QuestionAlreadyExistException extends Exception {
+    public QuestionAlreadyExistException(String message) {
+        super(message);
+    }
+}
