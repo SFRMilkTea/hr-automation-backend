757ff41408aa83d6cba10e792fd41dd547bdf1c7
SFRMilkTea <bayliz@bk.ru>
2022-12-18 15:21:52 +0700

    Добавила стили в экселе

--- src/main/java/com/example/hrautomationbackend/service/ExcelService.java
+++ src/main/java/com/example/hrautomationbackend/service/ExcelService.java
@@ -1,11 +1,13 @@
 package com.example.hrautomationbackend.service;
 
 import com.example.hrautomationbackend.entity.ProductEntity;
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -25,13 +27,20 @@ public class ExcelService {
     public byte[] createExcel() throws IOException {
         HSSFWorkbook workbook = new HSSFWorkbook();
         HSSFSheet sheet = workbook.createSheet("Лист 1");
+        CellStyle titleStyle = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setBold(true);
+        titleStyle.setFont(font);
         int rowNum = 0;
         Row row = sheet.createRow(rowNum);
         Cell nameCell = row.createCell(0);
+        nameCell.setCellStyle(titleStyle);
         nameCell.setCellValue("Название");
         Cell codeCell = row.createCell(1);
+        codeCell.setCellStyle(titleStyle);
         codeCell.setCellValue("Артикул");
         Cell quantityCell = row.createCell(2);
+        quantityCell.setCellStyle(titleStyle);
         quantityCell.setCellValue("Количество");
         List<ProductEntity> products = productService.getOrderedProducts();
         for (ProductEntity product : products) {
@@ -44,14 +53,15 @@ public class ExcelService {
             Cell cell2 = row1.createCell(2);
             cell2.setCellValue(product.getQuantity());
         }
-
+        sheet.autoSizeColumn(0);
+        sheet.autoSizeColumn(1);
+        sheet.autoSizeColumn(2);
         File f = new File("ordered_products.xls");
         f.createNewFile();
         FileOutputStream out = new FileOutputStream(f);
         workbook.write(out);
 
-        byte[] bytes = FileUtils.readFileToByteArray(f);
-        return bytes;
+        return FileUtils.readFileToByteArray(f);
 
     }
 }
