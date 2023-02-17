7c61f15150a5e9dbc50f013ec377aa05f43aeef6
SFRMilkTea <bayliz@bk.ru>
2023-02-17 22:33:11 +0700

    Создана сущность RestaurantResponse

---
+++ src/main/java/com/example/hrautomationbackend/model/RestaurantResponse.java
@@ -0,0 +1,26 @@
+package com.example.hrautomationbackend.model;
+
+public class RestaurantResponse {
+    private String address;
+    private String name;
+
+    public RestaurantResponse() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
