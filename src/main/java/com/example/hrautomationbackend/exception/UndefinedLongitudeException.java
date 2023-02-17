9f9ccfc05c569931a09878b266fbe746af90421a
SFRMilkTea <bayliz@bk.ru>
2023-02-17 22:07:58 +0700

    Добавлены ошибки "Невозможно определить широту/долготу"

---
+++ src/main/java/com/example/hrautomationbackend/exception/UndefinedLongitudeException.java
@@ -0,0 +1,7 @@
+package com.example.hrautomationbackend.exception;
+
+public class UndefinedLongitudeException extends Exception {
+    public UndefinedLongitudeException(String message) {
+        super(message);
+    }
+}
